41b198b7ebcb153bef71389fa0d530da9ddc9b4e
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-09-29 19:38:55 -0300

    Usando let e exibindo no console

---
+++ js/comuns.js
@@ -0,0 +1,15 @@
+//escrevendo no console
+console.log("Olá, eu sou o console");
+console.log("Posso te ajudar");
+
+let nomeUsuario = 'Israel Marques';
+let idadeUsuario = 50;
+let email = 'profisrael.copi@fiap.com';
+let noite = true;
+
+
+console.log(nomeUsuario);
+console.log(idadeUsuario);
+console.log(email);
+console.log(noite);
+
