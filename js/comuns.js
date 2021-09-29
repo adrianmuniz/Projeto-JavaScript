025b91f302b63dfea4f91158ede2ca091dfcada8
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-09-29 20:05:37 -0300

    Operadores relacionais e Arrays

--- js/comuns.js
+++ js/comuns.js
@@ -13,3 +13,27 @@ console.log(idadeUsuario);
 console.log(email);
 console.log(noite);
 
+console.log(typeof nomeUsuario, nomeUsuario);
+console.log(typeof(email), email);
+
+const dataNascimento = '02/12/1970';
+console.log(dataNascimento);
+
+console.log(`Usuario: ${nomeUsuario}`);
+
+let n1 = 10;
+let n2 = 2;
+console.log(`${n1} + ${n2} = ${n1 + n2}`);
+console.log(`${n1} - ${n2} = ${n1 - n2}`);
+console.log(`${n1} * ${n2} = ${n1 * n2}`);
+console.log(`${n1} / ${n2} = ${n1 / n2}`);
+console.log(`${n1} % ${n2} = ${n1 % n2}`);
+
+
+const pessoas = ['Eu', 'Tu', 'Ele', 'Nós', 'Vós','Eles'];
+
+console.log(pessoas);
+console.log(pessoas.length);
+console.table(pessoas);
+
+
